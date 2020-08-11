5aecd12d3244ee9b8f5965857754e3a408d7e97b
T.Oka <chita123daa@gmail.com>
2020-08-11 13:34:51 +0900

    topページ作成 検索バーとタイトル表示

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <title>画像検索</title>
+  </head>
+  <body>
+    <h1>海洋生物画像検索</h1>
+    <form>
+      <input type="text" name="search" value="" placeholder="生物の名前または種類">
+      <input type="submit" name="submit" value="検索">
+    </form>
+  </body>
