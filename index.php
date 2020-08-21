a523f526b84956298c9082b96918fab61f57439f
T.Oka <chita123daa@gmail.com>
2020-08-21 09:17:25 +0900

    検索前、検索後の表示機能変更　　検索機能のロジック

--- index.php
+++ index.php
@@ -9,7 +9,12 @@
   </head>
   <body>
     <div id="logo">
-      <h1>海洋生物画像検索</h1>
+<?php
+  if(empty($_GET[search])){
+    echo "<h1>海洋生物画像検索</h1>";
+  }
+?>
+      
     </div>
     <div id="fake-box">
       <form>
@@ -19,36 +24,39 @@
         </div>
       </form>
     </div>
+    <div class="disp-box">
+<?php
+  if(!empty($_GET[search])){
+    echo "<h2>" . htmlspecialchars($_GET[search]) . "の検索結果</h2>";
+  }
+?>
+    </div>
     <ul id="image-list">
 <?php
+error_reporting(E_ALL);
 // $keywords
 // $imgList
-require_once('./data/tag.php');
-//ディレクトリ名
+$search = '';
+$search = htmlspecialchars($_GET[search]);
+require('./data/tag.php');
 $dir_path = "./img";
 if (is_dir($dir_path)) {
   if(is_readable($dir_path)) { 
-    // ? ファイルが読み込み可能かどうか
-    $ch_dir = dir($dir_path); //ディレクトリクラス
-    //ディレクトリ内の画像を一覧表示
-    while (false !== ($file_name = $ch_dir -> read())) {
-      $ln_path = $ch_dir -> path . "/" .$file_name;
-        if (@getimagesize($ln_path)) { 
+    for ($i=0; $i < count($imgList) ; $i++) { 
+      for ($cnt=0; $cnt < count($imgList[$i][1]) ; $cnt++) { 
+        if ($search == $imgList[$i][1][$cnt]) {
           echo "<li class = \"".borderbox."\">";
-          echo "<a href = \"imgview.php?d=" .urlencode(mb_convert_encoding($ln_path, "UTF-8")). "\" target = \"_blank\" class= \"" .modal. "\" >";
-          echo "<img src = \"" .$ln_path. "\" width=\"250\" height=\"180\" ></a> ";
+          echo "<a href = " .urlencode(mb_convert_encoding($imgList[$i][0], "UTF-8")). "\" target = \"_blank\" class= \"" .modal. "\" >";
+          echo "<img src = \"" .$imgList[$i][0]. "\" width=\"250\" height=\"180\" ></a> ";
           echo "</li>";
         }
       }
-      $ch_dir -> close();
-    } else
-    {
-    echo "<p>" .htmlspecialchars($dir_path)."　は読み込みが許可されていません。";
     }
+  } else {
+    echo "<p>" .htmlspecialchars($dir_path)."　は読み込みが許可されていません。</p>";
     }
-    else
-    {
-    echo 'DIR 画像がないよ';
+} else {
+  echo "<p>画像が保存されてません</p>";
 }
 ?>
     </ul>
